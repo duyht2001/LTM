181905b46870287e4d25bdbc96f94c9df1635a64
duyht2001 <hoangthaiduy@hotmail.com>
2022-04-04 19:05:35 +0700

    update lan 2

--- Cilent/src/duy_61133538/Cilent.java
+++ Cilent/src/duy_61133538/Cilent.java
@@ -1,5 +1,53 @@
 package duy_61133538;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
 public class Cilent {
+	Socket socketClient;
+	int id = -1;
+	
+
+	public Cilent(Socket socketClient, int id) {
+		super();
+		this.socketClient = socketClient;
+		this.id = id;
+		
+	}
+
+
+
+	@Override
+	public void run() {
+		try {
+			System.out.print(socketClient.getInetAddress().getHostAddress());
+			System.out.print(id);
+			OutputStream osToClient = socketClient.getOutputStream();	
+			OutputStreamWriter write2Client = new OutputStreamWriter(osToClient);
+			BufferedWriter buffW = new BufferedWriter(write2Client);
+		
+			InputStream in = socketClient.getInputStream();
+			InputStreamReader inReader = new InputStreamReader(in);
+			BufferedReader buffR = new BufferedReader(inReader);
+			while(true) {
+			String chuoiNhan = buffR.readLine();
+			//Gửi trả
+			String chuoiGui = chuoiNhan;
+			buffW.write(chuoiGui+"\n");
+			buffW.flush();
+			if(chuoiGui.equals("Bye")) break;
+			}
+			socketClient.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	
+}
 
 }
--- Sever/src/duy_61133538/Sever.java
+++ Sever/src/duy_61133538/Sever.java
@@ -1,5 +1,20 @@
 package duy_61133538;
 
-public class Sever {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Sever {
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		ServerSocket socketServer = new ServerSocket(8686);
+		System.out.print("Tôi lắng nghe trong cổng 8686  ...");
+		int id =0;
+		while(true) {
+			Socket s = socketServer.accept();
+			//new Luong(s, id++).start();
+			Luong luongTask = new Luong(s, id);
+			luongTask.start();
+		}
+	}
 }
