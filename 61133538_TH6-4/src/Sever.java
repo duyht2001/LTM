886b3e49b4499ee71c289f86b809086c6a08711b
duyht2001 <hoangthaiduy@hotmail.com>
2022-04-06 19:12:43 +0700

    update lan 2

---
+++ 61133538_TH6-4/src/Sever.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Sever {
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		ServerSocket socketServer = new ServerSocket(9999);
+		System.out.print("Tôi lắng nghe trong cổng 9999  ...");
+		int id =0;
+		while(true) {
+			Socket s = socketServer.accept();
+			LuongLamViec_DocFile luongTask = new LuongLamViec_DocFile(s, id);
+			luongTask.start();
+		}
+	}
+}
